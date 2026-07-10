35183ff231e286be5f770b687793e76c9bc03819
prasetyama hidayat <prasetyama.hidayat@nirwanalestari.com>
2026-07-10 10:19:55 +0700

    remove middleware system log

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -43,7 +43,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'auth_sso.middleware.JWTAuthMiddleware',
-    'core.middleware.APIAuditMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
